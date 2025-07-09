9f142905f9ac34f2fee92e8135b17524bc5f392d
Litto K Lal <littoklal@gmail.com>
2025-07-09 11:24:05 +0530

    analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BlogList from './BlogList';
 import BlogDetail from './BlogDetail';
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
